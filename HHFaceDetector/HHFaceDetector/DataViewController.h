72fa381bf4d2c6a615ca37398d00207a12746615
hanjize <hanjize@rd.tuan800.com>
2014-12-26 15:53:13 +0800

    create
    create dic and iOS project

---
+++ HHFaceDetector/HHFaceDetector/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  HHFaceDetector
+//
+//  Created by LeAustinHan on 14-12-26.
+//  Copyright (c) 2014年 han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
---
+++ HHFaceDetector/HHFaceDetector/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  HHFaceDetector
+//
+//  Created by LeAustinHan on 14-12-26.
+//  Copyright (c) 2014年 han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController <UIPageViewControllerDelegate>
+
+@property (strong, nonatomic) UIPageViewController *pageViewController;
+
+@end
+
